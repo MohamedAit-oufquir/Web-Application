ef56e819341ff751526badf08257f3f749c75458
MohamedAit-oufquir <154386342+MohamedAit-oufquir@users.noreply.github.com>
2025-06-08 14:17:05 -0400

    setimeout 2sec

--- script.js
+++ script.js
@@ -10,7 +10,10 @@ recipeCloseBtn.addEventListener("click", () => {
   mealDetailsContent.parentElement.classList.remove("showRecipe");
 });
 
+
 // get meal list that matches with the ingredients
+
+
 function getMealList() {
   let searchInputTxt = document.getElementById("search-input").value.trim();
   fetch(
@@ -48,9 +51,11 @@ function getMealRecipe(e) {
   e.preventDefault();
   if (e.target.classList.contains("recipe-btn")) {
     let mealItem = e.target.parentElement.parentElement;
-    fetch(`www.themealdb.com/api/json/v1/1/search.php?f=a`)
-      .then((response) => response.json())
-      .then((data) => mealRecipeModal(data.meals));
+    setTimeout(() => {
+      fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=a`)
+        .then((response) => response.json())
+        .then((data) => mealRecipeModal(data.meals));
+    }, 2000); 
   }
 }
 
